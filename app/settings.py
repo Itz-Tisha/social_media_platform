1ac27e1b244801fe6e6b307a9e213a1bc9504756
Tisha <tishagoyani3@gmail.com>
2025-06-23 13:52:53 +0530

    fix: added render domain to ALLOWED_HOSTS

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-dev-secret-key')
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+
 
 
 # Application definition
